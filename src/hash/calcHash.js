d4870e93b9971c03dc739a82f723aa4ed496a97c
oleg <58697249+zerex21@users.noreply.github.com>
2024-01-25 15:48:46 +0300

    feat:completed folder hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import {
+    createReadStream
+} from 'node:fs';
+import {
+    stdout
+} from 'node:process';
+const {
+    createHash
+} = await import('node:crypto');
+
+const fileForHash = new URL('./files/fileToCalculateHashFor.txt',
+    import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const hash = createHash('sha256');
+    const input = createReadStream(fileForHash);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
+
+    // Write your code here
 };
 
 await calculateHash();
