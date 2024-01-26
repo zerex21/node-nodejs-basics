24744126bde34684cbe8713567af3544a836f20e
oleg <58697249+zerex21@users.noreply.github.com>
2024-01-26 15:00:32 +0300

    feat: completed folder zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import {
+    pipeline
+} from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream
+} from 'node:fs';
+import {
+    createGzip
+} from 'node:zlib';
+
+const pathFileZip = ['./files/fileToCompress.txt', '../archive.gz']
+
 const compress = async () => {
-    // Write your code here 
+
+    pipeline(createReadStream(pathFileZip[0]), createGzip(), createWriteStream(pathFileZip[1]), (err) => {
+        if (err) throw err
+    });
+    // Write your code here
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import {
+    pipeline
+} from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream
+} from 'node:fs';
+import {
+    createGunzip
+} from 'node:zlib';
+
+const pathFileZip = ['../fileToCompress.txt', '../archive.gz']
+
 const decompress = async () => {
-    // Write your code here 
+
+    pipeline(createReadStream(pathFileZip[1]), createGunzip(), createWriteStream(pathFileZip[0]), (err) => {
+        if (err) throw err
+    })
+    // Write your code here
 };
 
 await decompress();
