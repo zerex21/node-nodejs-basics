c2b9479c6a624a6ff26607727c5f7bb9c03e48d6
oleg <58697249+zerex21@users.noreply.github.com>
2024-01-29 19:07:58 +0300

    feat: completed folder wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,19 +3,6 @@ import {
     workerData
 } from 'node:worker_threads';
 
-/* import {
-    fileURLToPath
-} from 'url';
-import {
-    dirname
-} from 'path'; */
-
-/* const __filename = fileURLToPath(
-    import.meta.url);
-
-const __dirname = dirname(__filename); */
-// n should be received from main thread
-
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 
